2e573c1d209a3e02a05a9ab68e9bd0f90d6f3405
Isha Kale <ishuu0504@gmail.com>
2024-08-28 17:36:57 +0530

    Added a file Hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+package com.mbi.leave.approval;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+public class Hello {
+		
+	 System.out.println("Email notification sent successfully.");
+
+}
